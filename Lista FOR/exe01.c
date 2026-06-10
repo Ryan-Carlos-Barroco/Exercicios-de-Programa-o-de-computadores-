fb1fa82eb9464354f1ab172cb5697bfa2449ef1b
Ryan-Carlos-Barroco <barrocorc@hotmail.com>
2026-06-10 09:37:13 -0300

    altera o arquivo de teste para uma pasta de exercicio e exercicio 01 e 02 foram adicionados a pasta FOR

---
+++ Lista FOR/exe01.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main (void){
+        
+        int i; 
+
+        for (i=2; i<=50; i+=2)
+        {
+                printf("%d\n",i);
+        }
+        
+        return 0;
+}
